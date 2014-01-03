098685734ec4c257302004e111b181e8ea9ff572
Gerald L. Hevener Jr <ghevener@customink.com>
2014-01-03 09:40:26 -0500

    Initial cookbook creation.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'ghubbkup'
+maintainer       'Gerald L. Hevener Jr., M.S.'
+maintainer_email 'jackl0phty@gmail.com'
+license          ''
+description      'Installs/Configures ghubbkup'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: ghubbkup
+# Recipe:: default
+#
+# Copyright 2014, Gerald L. Hevener Jr., M.S.
+#
