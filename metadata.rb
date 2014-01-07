b1c8e44e2e10e08d4c33ca9b455be2a7e881ea95
Gerald L. Hevener Jr <ghevener@customink.com>
2014-01-07 14:04:00 -0500

    make conf_dir a global CONSTANT. bump.

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'jackl0phty@gmail.com'
 license          ''
 description      'Installs/Configures ghubbkup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.12'
+version          '0.1.13'
